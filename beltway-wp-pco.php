8ba9394a75ea20b96dd1f358a0ee731abe5a265b
neverarriving <nvisiondesign@gmail.com>
2014-01-14 23:26:09 -0800

    Add settings link to plugin description

--- beltway-wp-pco.php
+++ beltway-wp-pco.php
@@ -17,3 +17,13 @@ require_once(dirname(__FILE__).'/admin.php');
 
 // scripts
 require_once('scripts/recentsinging.php');
+
+function beltway_plugin_action_links( $links, $file ) {
+	$plugin_file = basename(__FILE__);
+	if (basename($file) == $plugin_file) {
+		$links[] = '<a href="' . admin_url( 'options-general.php?page=pco_connect' ) . '">'.__( 'Settings' ).'</a>';
+	}
+	return $links;
+}
+
+add_filter( 'plugin_action_links', 'beltway_plugin_action_links', 10, 2 );
